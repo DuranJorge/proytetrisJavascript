6e6c0d2433df8fda4bb444be7aca3e43e4a9e2d5
Jorge Duran <jldb6962016@gmail.com>
2019-08-30 20:20:00 -0500

    Se actualizo para el servidor coordine con heroku

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var app = require('express')();
+/*var app = require('express')();
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});*/
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(port, function(){
